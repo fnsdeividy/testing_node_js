3566de15c05fa416018d54e3b8ba7d8eeffca6f2
deividy <deividyferreira.n@gmail.com>
2021-11-05 10:26:19 -0300

    add show profile

--- src/Controller/index.js
+++ src/Controller/index.js
@@ -4,10 +4,10 @@ let users = [];
 
 module.exports = {
     createProfile(req, res) {
-        const Id = uuid()
+        
         const { name, age, favorite_artist, favorite_work } = req.body;
         const user = {
-        id: Id,
+        id: uuid(),
         name,
         age,
         favorite_artist,
@@ -23,5 +23,10 @@ module.exports = {
     
 
     return res.json(user);
-  },    
+  },
+  showProfile(req, res) {
+    const user = users.find((user) => user.id === req.params.id);
+
+    return res.json(user);
+  },
 }
--- src/server.js
+++ src/server.js
@@ -7,5 +7,6 @@ const PORT = 4000
 app.use(express.json())
 
 app.post('/profile', controller.createProfile)
+app.get('/profile/:id', controller.showProfile)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
